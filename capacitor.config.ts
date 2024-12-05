c18f793c4732882663e6861279bf1c63b12106da
ZiaKfa <zia14148@gmail.com>
2024-12-05 07:51:21 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Responsi Mobile',
+  webDir: 'www'
+};
+
+export default config;
